24cf6059a7a802f8b8bb7447d2b41244cd3741c3
macus <maryncevigor@gmail.com>
2024-01-22 17:01:31 +0200

    lesson

--- lesson_2.0.py
+++ lesson_2.0.py
@@ -1,2 +1,5 @@
-print ('hello world')
-print ('ok')
+name = input('Please enter four dight number, from 0 to 9:')
+name_1 = ('Hi, here is your result.')
+
+digit_1 = int(name) // 1000
+digit_2 = int(name) // 100 % 10
